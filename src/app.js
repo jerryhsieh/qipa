35593b554622281630088bc1b301c15ffde12ab5
Jerry Hsieh <jerryhsieh01@gmail.com>
2018-03-28 19:30:17 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,5 @@
+
+import $ from 'jquery';
+import 'bootstrap';
+import './scss/app.scss';
+import 'lodash';
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        include: path.resolve(__dirname, 'src/templates'),
+        loader: 'html-loader',
+        options: {
+          interpolate: true
+        }
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: {
+          loader: 'responsive-loader',
+          options: {
+            sizes: [1000],
+            placeholder: true,
+            placeholderSize: 50,
+            name: 'imgs/[name]-[width].[ext]'
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader', // inject CSS to page
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS modules
+        }, {
+          loader: 'postcss-loader', // Run post css actions
+          options: {
+            plugins: function () { // post css plugins, can be exported to postcss.config.js
+              return [
+                require('precss'),
+                require('autoprefixer')
+              ];
+            }
+          }
+        }, {
+          loader: 'sass-loader' // compiles Sass to CSS
+        }]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new ImageminPlugin({test: /\.(png|jpg|gif)$/})
+  ]
+};
